ece9994dab18f23dc163a737c6d0e9d08a2b37c5
hiyoko <62996611+hiyoko9513@users.noreply.github.com>
2025-01-02 09:44:02 +0900

    :recycle: fmt実行

--- aws/services/hiyoko/develop/vpc.tf
+++ aws/services/hiyoko/develop/vpc.tf
@@ -2,28 +2,28 @@
 # VPC
 # ====================================================
 module "vpc_main" {
-  source         = "../modules/vpc"
-  vpc_cidr       = var.vpc_cidr
-  project        = var.project
-  environment    = var.environment
+  source      = "../modules/vpc"
+  vpc_cidr    = var.vpc_cidr
+  project     = var.project
+  environment = var.environment
   public_subnets = [
     {
       availability_zone = var.availability_zone["a"]
-      cidr             = var.subnets_cidr["public_a"]
+      cidr              = var.subnets_cidr["public_a"]
     },
     {
       availability_zone = var.availability_zone["c"]
-      cidr             = var.subnets_cidr["public_c"]
+      cidr              = var.subnets_cidr["public_c"]
     }
   ]
   private_subnets = [
     {
       availability_zone = var.availability_zone["a"]
-      cidr             = var.subnets_cidr["private_a"]
+      cidr              = var.subnets_cidr["private_a"]
     },
     {
       availability_zone = var.availability_zone["c"]
-      cidr             = var.subnets_cidr["private_c"]
+      cidr              = var.subnets_cidr["private_c"]
     }
   ]
 }
