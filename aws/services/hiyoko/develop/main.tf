c90123138639579e537d5c2b0513341f89a6d2da
hiyoko <62996611+hiyoko9513@users.noreply.github.com>
2025-01-03 14:05:47 +0900

    :wrench: プロファイルの削除

--- aws/services/hiyoko/develop/main.tf
+++ aws/services/hiyoko/develop/main.tf
@@ -15,6 +15,5 @@ terraform {
 # TODO: narikawa .envからregionを取得できない？
 # TODO: narikawa tfvarsはgit管理しない場合、どうやって生成する？（サンプルファイルでもおいておく？）
 provider "aws" {
-  profile = "hiyoko-terraform"
   region  = "ap-northeast-1"
 }
