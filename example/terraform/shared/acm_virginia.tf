d26f42e6b059a0bfd949e0fec9aa379d50122383
hiyoko <62996611+hiyoko9513@users.noreply.github.com>
2025-02-06 17:51:04 +0900

    :wrench: virginia用のacm追加

---
+++ example/terraform/shared/acm_virginia.tf
@@ -0,0 +1,28 @@
+# ====================================================
+# virginia region
+# ====================================================
+provider "aws" {
+  alias = "virginia"
+  profile = "terraform"
+  region = "us-east-1"
+}
+
+resource "aws_acm_certificate" "virginia" {
+  provider = aws.virginia
+  domain_name       = "*.${var.domain}"
+  validation_method = "DNS"
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-wikdecard-sslcert"
+    Project = var.project
+    Env     = var.environment
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [
+    aws_route53_zone.main
+  ]
+}
